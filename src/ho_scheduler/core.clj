85427f433e114ef16cad6ee2ec8c2d0a523f22a0
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-05-13 21:45:50 +0100

    Cleanup.

--- src/ho_scheduler/core.clj
+++ src/ho_scheduler/core.clj
@@ -8,13 +8,9 @@
 (def ho-server-url "https://UNF6X7OJB7PFLVEQ.anvil.app/_/private_api/HN4JZ6UMWUM7I4PTILWZTJFD/league/supported")
 
 (defn -main [& _args]
-  (let [doc (-> (Jsoup/connect hattrick-url)
-                .get)
-        element (-> (.select doc "#online")
-                    .text)
+  (let [doc (-> (Jsoup/connect hattrick-url) .get)
+        element (-> (.select doc "#online") .text)
         m (last (re-matches #".*week ([0-9]+)" element))]
     (when (>= (Integer/parseInt m) 14)
       (println
-       (:body (client/get
-               ho-server-url
-               {:accept :json}))))))
+       (:body (client/get ho-server-url {:accept :json}))))))
